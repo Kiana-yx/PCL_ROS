da9feac3ca5dcd62c899712b9e551f1fb0323ec1
Kiana <1436789500@qq.com>
2022-02-17 12:47:31 +0800

    fail1.0

--- src/camera_pkg/scripts/face_follow.py
+++ src/camera_pkg/scripts/face_follow.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import rospy
 from sensor_msgs.msg import Image
 import cv2, cv_bridge
@@ -8,7 +11,6 @@ face_cascade = cv2.CascadeClassifier( '/home/kiana/catkin_ws/src/camera_pkg/scri
 class Follower:
     def __init__(self):
         self.bridge = cv_bridge.CvBridge()
-        cv2.namedWindow("window", cv2.WINDOW_NORMAL)
         self.image_sub = rospy.Subscriber("/camera/color/image_raw", Image, self.image_callback)
 
     def image_callback(self, msg):
@@ -25,7 +27,26 @@ class Follower:
             #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', image)
+        # cv2.waitKey(1)
+        # cv2.destroyAllWindows()
 
-rospy.init_node("opencv")
-follower = Follower()
-rospy.spin()
+def fase_follow():
+	# ROS节点初始化
+    rospy.init_node('opencv', anonymous=True)
+
+	#设置循环的频率
+    rate = rospy.Rate(10) 
+
+    while not rospy.is_shutdown():
+		# 初始化geometry_msgs::Twist类型的消息
+        follower = Follower()
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+		# 按照循环频率延时
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        fase_follow()
+    except rospy.ROSInterruptException:
+        pass
