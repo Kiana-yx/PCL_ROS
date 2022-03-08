a6015d1105a690706d70c2c53c80d8f213ed1bd2
Kiana <1436789500@qq.com>
2022-03-08 17:48:08 +0800

    pub grid_map 1.0

--- src/map_pkg/include/grid_core.h
+++ src/map_pkg/include/grid_core.h
@@ -7,6 +7,7 @@
 #include <pcl/point_types.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl/filters/project_inliers.h>
+#include <pcl/filters/voxel_grid.h>
 #include <pcl_conversions/pcl_conversions.h>
 
 #define PI 3.1415926
@@ -15,16 +16,27 @@ class GridCore
 {
 
 private:
+    float grid_size = 0.1;
+    float map_x0 = 0;
+    float map_y0 = -10;
+    float map_z0 = 0;
+    int width_x = round(10 / grid_size);
+    int width_y = round(20 / grid_size);
+
     ros::Subscriber sub_point_cloud_; //接受话题
 
     ros::Publisher pub_grid_, pub_2d_; //发布话题
 
+    std_msgs::Header point_cloud_header_;
+
     void publish_cloud(const ros::Publisher &in_publisher,
-                                const pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud_to_publish_ptr,
-                                const std_msgs::Header &in_header);
-    void grid_main(const sensor_msgs::PointCloud2ConstPtr &in_cloud_ptr);
+                       const pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud_to_publish_ptr,
+                       const std_msgs::Header &in_header);
+
     void Point_2D(const pcl::PointCloud<pcl::PointXYZ>::Ptr in,
                   const pcl::PointCloud<pcl::PointXYZ>::Ptr out);
+    void grid_create(const pcl::PointCloud<pcl::PointXYZ>::Ptr in);
+    void grid_main(const sensor_msgs::PointCloud2ConstPtr &in_cloud_ptr);
 
 public:
     GridCore(ros::NodeHandle &nh);
--- src/map_pkg/src/grid_core.cpp
+++ src/map_pkg/src/grid_core.cpp
@@ -8,7 +8,7 @@ GridCore::GridCore(ros::NodeHandle &nh)
     pub_grid_ = nh.advertise<nav_msgs::OccupancyGrid>("/grid_points", 10);
 
     //程序到达ros::spin()之前按照一系列规则，设定一系列话题订阅者
-    //ros::spin()使订阅者们可以开始接受话题，进入回调函数
+    // ros::spin()使订阅者们可以开始接受话题，进入回调函数
     ros::spin();
 }
 
@@ -19,8 +19,8 @@ void GridCore::Spin()
 }
 
 void GridCore::publish_cloud(const ros::Publisher &in_publisher,
-                                const pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud_to_publish_ptr,
-                                const std_msgs::Header &in_header)
+                             const pcl::PointCloud<pcl::PointXYZ>::Ptr in_cloud_to_publish_ptr,
+                             const std_msgs::Header &in_header)
 {
     sensor_msgs::PointCloud2 cloud_msg;
     pcl::toROSMsg(*in_cloud_to_publish_ptr, cloud_msg);
@@ -29,61 +29,72 @@ void GridCore::publish_cloud(const ros::Publisher &in_publisher,
 }
 
 void GridCore::Point_2D(const pcl::PointCloud<pcl::PointXYZ>::Ptr in,
-                  const pcl::PointCloud<pcl::PointXYZ>::Ptr out)
+                        const pcl::PointCloud<pcl::PointXYZ>::Ptr out)
 {
     pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
-	coefficients->values.push_back(0.0);
-	coefficients->values.push_back(0.0);
-	coefficients->values.push_back(1.0);
-	coefficients->values.push_back(0.0);
+    coefficients->values.push_back(0.0);
+    coefficients->values.push_back(0.0);
+    coefficients->values.push_back(1.0);
+    coefficients->values.push_back(0.0);
 
-	pcl::ProjectInliers<pcl::PointXYZ> proj;
-	proj.setInputCloud(in);
-	proj.setModelCoefficients(coefficients);
-	proj.setModelType(pcl::SACMODEL_PLANE);
-	proj.filter(*out);
+    pcl::ProjectInliers<pcl::PointXYZ> proj;
+    proj.setInputCloud(in);
+    proj.setModelCoefficients(coefficients);
+    proj.setModelType(pcl::SACMODEL_PLANE);
+    proj.filter(*out);
+}
 
+void GridCore::grid_create(const pcl::PointCloud<pcl::PointXYZ>::Ptr in)
+{
+    // 定义地图对象
+    nav_msgs::OccupancyGrid myMap;
+
+    // 地图数据
+    myMap.info.map_load_time = ros::Time(0);
+    myMap.info.resolution = grid_size;
+    myMap.info.width = width_x;
+    myMap.info.height = width_y;
+    myMap.info.origin.position.x = map_x0;
+    myMap.info.origin.position.y = map_y0;
+    myMap.info.origin.position.z = map_z0;
+    myMap.info.origin.orientation.x = 0;
+    myMap.info.origin.orientation.y = 0;
+    myMap.info.origin.orientation.z = 0;
+    myMap.info.origin.orientation.w = 1;
+
+    int p[myMap.info.width * myMap.info.height] = {-1}; // [0,100]
+
+    for (int count = 0; count < in->points.size(); count++)
+    {
+        int x_grid = round((in->points[count].x - map_x0) / grid_size) + 1;
+        int y_grid = round((in->points[count].y - map_y0) / grid_size) + 1;
+
+        int grid = (y_grid - 1) * width_x + x_grid - 1;
+        p[grid]=100;
+    }
+
+    std::vector<signed char> a(p, p + width_x*width_y);
+    myMap.data = a;
+
+    // frame id
+    myMap.header.frame_id = "velodyne";
+    // 广播
+    pub_grid_.publish(myMap);
 }
 
 void GridCore::grid_main(const sensor_msgs::PointCloud2ConstPtr &in_cloud_ptr)
 {
     pcl::PointCloud<pcl::PointXYZ>::Ptr current_3D_ptr(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::PointXYZ>::Ptr point_2D_ptr(new pcl::PointCloud<pcl::PointXYZ>);
-    
+    point_cloud_header_ = in_cloud_ptr->header;
     pcl::fromROSMsg(*in_cloud_ptr, *current_3D_ptr);
 
-    Point_2D(current_3D_ptr,point_2D_ptr);
+    pcl::VoxelGrid<pcl::PointXYZ> filter;
+    filter.setInputCloud(current_3D_ptr);
+    filter.setLeafSize(grid_size, grid_size, 2 * grid_size);
+    filter.filter(*current_3D_ptr);
+
+    Point_2D(current_3D_ptr, point_2D_ptr);
     publish_cloud(pub_2d_, point_2D_ptr, in_cloud_ptr->header);
-
-    // 定义地图对象
-    nav_msgs::OccupancyGrid myMap;
-
-    // 地图数据 
-    myMap.info.map_load_time = ros::Time(0);
-    myMap.info.resolution = 0.5;
-    myMap.info.width = 10;
-    myMap.info.height = 20;
-    myMap.info.origin.position.x = 0;
-    myMap.info.origin.position.y = 0;
-    myMap.info.origin.position.z = 0;
-    myMap.info.origin.orientation.x = 0;
-    myMap.info.origin.orientation.y = 0;
-    myMap.info.origin.orientation.z = 0;
-    myMap.info.origin.orientation.w = 1;
-
-    for (std::size_t i = 0; i < myMap.info.width * myMap.info.height; i++)
-    {
-        int8_t a;
-        if(i==2||i==11||i==20||i==9)
-        {a=200;}
-        else
-        {a = 1;}
-        myMap.data.push_back(a);
-    }
-
-    // frame id
-    myMap.header.frame_id = "velodyne";
-    // 广播
-	pub_grid_.publish(myMap);
-
+    grid_create(point_2D_ptr);
 }
